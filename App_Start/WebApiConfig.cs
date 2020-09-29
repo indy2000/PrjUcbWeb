364916fe2bbb7825fb7ceb312a703b3fd422358f
Yago Dias <yago@sinapseinformatica.com.br>
2020-09-29 01:22:06 -0300

    PrjUcbWeb v1.0.0 - Publicado

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace PrjUcbWeb
 {
@@ -12,6 +13,9 @@ namespace PrjUcbWeb
         public static void Register(HttpConfiguration config)
         {
             // Serviços e configuração da API da Web
+
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
 
             // Rotas da API da Web
             config.MapHttpAttributeRoutes();
